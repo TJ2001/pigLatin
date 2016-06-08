103fee3ee5501353fb75edc91976e234ee136891
Maarja Laineste and Tim Jung <tim@>
2016-06-08 14:09:43 -0700

    add spec for checking consonant as first letter

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,65 @@
-var arrVowel = ["a", "e", "o", "u", "i"];
-
-var arrWord = word.split('');
+// var wordArray = word.split("");
 var pigLatin = function(word) {
-  var output = word + "ay";
-  for (var i = 0; i < word.length; i++) {
-    //we want to look at "c" => word[0] => word[i]
-    for (var j = 0; j < arrVowel.length; j++) {
-      if (word.charAt(i) !== arrVowel[j]) {
-        //
-        return word.slice(0, -1);
+  debugger;
+  var arrVowel = ["a", "e", "o", "u", "i"];
+  var newArr = word.split('');
 
-        } else {
+  var output = newArr + "ay";
 
-      }
+  for (var i = 0; i < arrVowel.length; i++) {
+    if (word.charAt(0) !== arrVowel[i]) {
+
+
+      newArr.shift();
+      newArr.push(word.charAt(0));
+      console.log(newArr);
+      newArr.join('');
+      console.log(newArr);
+      output = newArr + "ay";
+      return output;
+
+    } else {
+      newArr.join('');
+      return output;
     }
   }
-  return output;
-}
+};
+
+
+
+
+
+
+
+
+
+// var arrVowel = ["a", "e", "o", "u", "i"];
+//
+// var arrWord = word.split('');
+// var pigLatin = function(word) {
+//   var output = word + "ay";
+//   for (var i = 0; i < word.length; i++) {
+//     //we want to look at "c" => word[0] => word[i]
+//     for (var j = 0; j < arrVowel.length; j++) {
+//       if (word.charAt(i) !== arrVowel[j]) {
+//         //
+//         return word.slice(0, -1);
+//
+//         } else {
+//
+//       }
+//     }
+//   }
+//   return output;
+// }
+
+
+
+
+
+
+
+
 
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
